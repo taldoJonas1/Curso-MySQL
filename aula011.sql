067f103e0206b7f23cb966cc695c581e7bc948af
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2023-10-24 14:10:07 -0300

    Update aula011.sql

--- aula011.sql
+++ aula011.sql
@@ -4,8 +4,10 @@ select
     cus.customer_id, 
     cus.first_name, 
     cus.last_name, 
+    adr.address,
     pay.rental_id,
     pay.amount
 from customer cus -- apelido ALIAS
 join payment pay on cus.customer_id = pay.payment_id
+join address adr on cus.customer_id = adr.address_id
 -- JOIN junta duas tabelas
